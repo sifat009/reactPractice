e685d7b48fc9d9983ce76d84d494fe8de211476c
sifat009 <bhacker150@gmail.com>
2019-05-19 13:46:39 +0600

    added the react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="Catch of the Day" />
                 </div>
                 <Order/>
                 <Inventory/>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
-import StorePicker from './components/StorePicker'; 
-import './css/style.css';
-import App from './components/App';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
 
-render(<App />, document.getElementById('root'));
+import StorePicker from './components/StorePicker'; 
+import App from './components/App';
+import NotFound from './components/NotFound';
+import './css/style.css';
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={StorePicker} />
+                <Route path="/store/:storeId" component={App} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+render(<Root />, document.getElementById('root'));
 
