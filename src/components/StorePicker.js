2fa0c2962097e1bce8eedfa0dcc567cc10713f15
sifat009 <bhacker150@gmail.com>
2019-05-22 07:06:02 +0600

    added half of gotoStore method, got the value of storeInput

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,16 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    gotoStore = (event) => {
+        event.preventDefault();
+        console.log(this.storeInput.value);
+
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.gotoStore}>
                 <h2>Please enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => this.storeInput = input} />
                 <button type="submit">Visit Store -></button>
             </form>
         )
